8ddeaa4df10a71b511c763990baa2aec185091af
diego-espindola <diegoespindola9404@gmail.com>
2022-06-19 22:25:59 -0300

    Finished Principal

--- src/Principal.java
+++ src/Principal.java
@@ -1,7 +1,16 @@
+import com.singleton.SingleObject;
+
 public class Principal {
     public static void main(String[] args) {
 
-        
+        //Illegal constructor
+        //SingleObject object = new SingleObject();
 
+        //Recover an object
+        SingleObject object1 = SingleObject.getInstance();
+        object1.showMessage();
+
+        SingleObject object2 = SingleObject.getInstance();
+        object2.showMessage();
     }
 }
